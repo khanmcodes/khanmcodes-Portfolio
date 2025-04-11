7fe7e01a588d85ac9429762ed6a62ca7bbf0fc19
khanmcodes <kkhalepoto@gmail.com>
2025-04-11 17:43:03 +0500

    Fixed grid bug

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,13 +3,13 @@ import Logo from "./Logo";
 export default function Topbar() {
   return (
       <div className="container px-2 pt-4 sm:px-12 flex items-center max-w-screen sm:justify-between">
-        <div className="text-xs font-normal hidden sm:flex">github.com/khanmcodes</div>
+        <div className="text-[10px] xl:text-xs font-normal hidden sm:flex">github.com/khanmcodes</div>
 
         <div>
           <Logo size={80} spinDuration={20} onHover="pause" />
         </div>
 
-        <div className="text-xs font-normal hidden sm:flex">khanmuhammad.work@gmail.com</div>
+        <div className="text-[10px] xl:text-xs font-normal hidden sm:flex">khanmuhammad.work@gmail.com</div>
       </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="mx-1">
       {/* Hero Section */}
-      <div className="flex items-end sm:ml-72">
+      <div className="flex items-end sm:ml-30 xl:ml-72">
         <div className="ml-1 mt-5 sm:mt-20">
           <div className="max-w-full flex flex-col">
             <h1 className="text-4xl mb-4">
@@ -17,14 +17,14 @@ export default function Home() {
               muhammad<span style={{ color: "var(--color-primary)" }}>.</span>
               <br />
             </h1>
-            <p className="text-xs sm:text-sm leading-loose sm:w-[30vw] font-extralight">
+            <p className="text-xs sm:text-sm leading-loose lg:w-[40vw] xl:w-[30vw] font-extralight">
               Creative designer and developer with a passion for building sleek,
               high-performance applications. Turning ideas into interactive
               experiences—one line of code at a time.
             </p>
           </div>
         </div>
-        <div className="hidden sm:flex gap-3 text-sm font-extralight justify-end w-3/5">
+        <div className="hidden sm:flex gap-3 text-sm font-extralight justify-end w-3/5 mr-10">
           scroll down{" "}
           <FaCaretDown color="var(--color-primary)" className="mt-0.5" />
         </div>
@@ -32,35 +32,31 @@ export default function Home() {
 
       {/* Projects Wall */}
       <div className="my-10 flex justify-center items-center">
-        <div className="grid grid-cols-2 sm:grid-cols-6 gap-1.5 w-[100vw]">
+        <div className="grid grid-cols-2 sm:grid-cols-6 gap-1.5 w-full">
           {/* First row */}
-          <div className="col-span-2 min-h-65 max-h-65 bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65 bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg "></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-2 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40 bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40 bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg "></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
           <div className="row-span-2 bg-neutral-900 rounded-lg"></div>
 
           {/* Second row */}
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="row-span-2  bg-neutral-900 rounded-lg"></div>
-          <div className="row-span-2 col-span-2 bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
+          <div className="hidden sm:block row-span-2  bg-neutral-900 rounded-lg"></div>
+          <div className="row-span-2 col-span-2 min-h-80 max-h-80 sm:min-h-0 sm:max-h-none bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
 
-          {/* <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="row-span-2  bg-neutral-800 rounded-lg"></div>
-          <div className="col-span-2 row-span-2  bg-neutral-800 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div> */}
 
           {/* Third row */}
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-2 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-2 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
 
           {/* Fourth row */}
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-2 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
-          <div className="col-span-1 min-h-65 max-h-65  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-2 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
+          <div className="col-span-1 xl:min-h-65 xl:max-h-65 sm:min-h-35 sm:max-h-35 2xl:min-h-75 2xl:max-h-75 min-h-40 max-h-40  bg-neutral-900 rounded-lg"></div>
         </div>
       </div>
     </div>
